55b10b4ba703053c58c5ea070e5dee3cbbbd485b
Sanket Singh <singhsanket143@gmail.com>
2023-11-28 22:49:58 +0530

    Added body parser configurations

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express'); // we got access to express function
+const bodyParser = require('body-parser');
 
 const app = express(); // app is a new express application object
 
 const PORT = 3000;
+
+
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.text()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 // Let's define some routes
 
@@ -11,6 +17,11 @@ app.get('/ping', (request, response) => {
     // Request object contains detail about incming request, details like query params, body params etc
     // Response object contains details about what response will be sent back to the client
     console.log("Ping received");
+
+    console.log(request.query);
+
+    console.log(request.body);
+
     response.json({message: 'PING received'});
 }); // 2 arguments , 1-> route as a string, 2-> callback
 
@@ -24,3 +35,11 @@ app.listen(PORT, () => {
     // Example DB connection
     console.log(`Server started at port ${PORT}`);
 }); // The listen method takes 2 parameter -> 1. Port number 2. Callback
+
+
+/**
+ * 
+ * 1. URL Params -> /products/1 or /products/2
+ * 2. Query Params -> /products?rating=4&min_price=30&max_price=100
+ * 3. Body Params -> 
+ */
